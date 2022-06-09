41f95a17c9bc59348faa67164e00b646f68f70fd
alexeyShuncko <shuncko.a@yandex.ru>
2022-06-09 11:41:04 +0300

    Крутилка

--- src/components/Login/ButtonLogin.js
+++ src/components/Login/ButtonLogin.js
@@ -4,59 +4,62 @@ import s from './Login.module.css';
 import { Form, Button, Space } from 'antd';
 import { connect } from 'react-redux';
 import { getUser } from './../../Redux/profileReducer';
+import  setting  from './../../image/Settings.gif';
 
 
-const ButtonLogin = ({getUser,...props}) => {
+
+const ButtonLogin = ({ getUser, ...props }) => {
 
 
   const [err, setErr] = useState(false)
 
   useEffect(() => {
     getUser()
-    .catch(() => setErr(true) )
-  },[getUser])
+      .catch(() => setErr(true))
+  }, [getUser])
 
   const redirect = (e) => {
-    
+
     e.currentTarget.innerText === 'Войти'
       ? props.updateLogin('login')
       : props.updateLogin('registr')
   }
 
   return (
-    <div  className={s.container}>
+    <div className={s.container}>
 
       {
         !err
-        ?<div className={s.shadow}>
-        <Form
-          size='large'
-          name="buttonLogin"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 12 }}
-          autoComplete="off"
-        >
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Space>
-              <Button type="primary" onClick={redirect}>
-                Войти
-              </Button>
-              <Button type="primary" danger onClick={redirect}>
-                Регистрация
-              </Button>
-            </Space>
-          </Form.Item>
-  
-        </Form>
-      </div>
-        : <div className={s.err}>
-Ведутся работы на сервере, попробуйте зайти позже...
-        </div>
+          ? <div className={s.shadow}>
+            <Form
+              size='large'
+              name="buttonLogin"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 12 }}
+              autoComplete="off"
+            >
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Space>
+                  <Button type="primary" onClick={redirect}>
+                    Войти
+                  </Button>
+                  <Button type="primary" danger onClick={redirect}>
+                    Регистрация
+                  </Button>
+                </Space>
+              </Form.Item>
+
+            </Form>
+          </div>
+          : <div className={s.err}>
+            Ведутся работы на сервере, попробуйте зайти позже...
+            <img alt='крутилка' src={setting}/>
+          </div>
       }
     </div>
   )
